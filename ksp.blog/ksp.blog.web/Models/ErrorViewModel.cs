0959079d352b0b93578f4eab410fda3861ae94eb
Farhan Kaioum <km1472233@gmail.com>
2020-09-22 23:56:44 -0700

    initial commit

---
+++ ksp.blog/ksp.blog.web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ksp.blog.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
