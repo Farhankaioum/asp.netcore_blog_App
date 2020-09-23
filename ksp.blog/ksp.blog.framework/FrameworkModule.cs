1c9a7e339fefc373b2f0d4b712eda61fd348e5bb
Farhan Kaioum <km1472233@gmail.com>
2020-09-23 00:13:07 -0700

    autofac configuration added for DI

---
+++ ksp.blog/ksp.blog.framework/FrameworkModule.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+
+namespace ksp.blog.framework
+{
+    public class FrameworkModule : Module
+    {
+        private readonly string _connectionString;
+        private readonly string _migrationAssemblyName;
+
+        public FrameworkModule(string connectionString, string migrationAssemblyName)
+        {
+            _connectionString = connectionString;
+            _migrationAssemblyName = migrationAssemblyName;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+
+            //builder.RegisterType<FrameworkContext>()
+            //    .WithParameter("connectionString", _connectionString)
+            //    .WithParameter("migrationAssemblyName", _migrationAssemblyName)
+            //    .InstancePerLifetimeScope();
+
+            base.Load(builder);
+        }
+    }
+}
--- ksp.blog/ksp.blog.web/Startup.cs
+++ ksp.blog/ksp.blog.web/Startup.cs
@@ -6,17 +6,42 @@ using ksp.blog.web.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using ksp.blog.framework;
 
 namespace ksp.blog.web
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        #region autofac configuration
+        public Startup(IWebHostEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+               .SetBasePath(env.ContentRootPath)
+               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+               .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+               .AddEnvironmentVariables();
+            this.Configuration = builder.Build();
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfigurationRoot Configuration { get; private set; }
+
+        public static ILifetimeScope AutofacContainer { get; private set; }
+
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            var connectionStringName = "DefaultConnection";
+            var connectionString = Configuration.GetConnectionString(connectionStringName);
+            var migrationAssemblyName = typeof(Startup).Assembly.FullName;
+
+
+            builder.RegisterModule(new FrameworkModule(connectionString, migrationAssemblyName));
+            //builder.RegisterModule(new WebModule(connectionString, migrationAssemblyName));
+
+        }
+        #endregion
+
 
         public void ConfigureServices(IServiceCollection services)
         {
@@ -27,10 +52,12 @@ namespace ksp.blog.web
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddOptions();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            AutofacContainer = app.ApplicationServices.GetAutofacRoot(); // for autofac
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
