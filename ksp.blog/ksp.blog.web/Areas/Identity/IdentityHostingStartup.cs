d637addbf75338da54a5d521db8956a1dd211190
Farhan Kaioum <km1472233@gmail.com>
2020-09-25 01:13:06 -0700

    Identity template added for Identity

---
+++ ksp.blog/ksp.blog.web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ksp.blog.membership.Contexts;
+using ksp.blog.membership.Entities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ksp.blog.web.Areas.Identity.IdentityHostingStartup))]
+namespace ksp.blog.web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
